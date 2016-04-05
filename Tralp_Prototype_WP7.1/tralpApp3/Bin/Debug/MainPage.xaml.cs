485b1b2b81913690615ac771e3fd3a375da5500b
anachicarino <anachicarino@hotmail.com>
2016-04-05 09:19:01 -0300

    Added files via upload

---
+++ Tralp_Prototype_WP7.1/tralpApp3/Bin/Debug/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿// Directives
+using Microsoft.Devices;
+using System.IO;
+using System.IO.IsolatedStorage;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace tralpApp3
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+
+        }
+
+
+        //EVENTS
+        //private void Livro_Click(object sender, EventArgs e)
+        //{
+        //    NavigationService.Navigate(new Uri("/Dicionario.xaml", UriKind.Relative));
+        //}
+
+        //NAVIGATION BARS
+        private void PivotMain_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (((Pivot)sender).SelectedIndex)
+            {
+                case 0:
+                    ApplicationBar = ((ApplicationBar)Application.Current.Resources["AppBar1"]);
+                    break;
+
+                case 1:
+                    ApplicationBar = ((ApplicationBar)Application.Current.Resources["AppBar2"]);
+                    break;
+            }
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        private void ApplicationBarIconButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Teclado_Click(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
---
+++ Tralp_Prototype_WP7.1/tralpApp3/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace tralpApp3
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void texto1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            texto1.Text = "";
+            
+        }
+
+        private void texto1_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (texto1.Text == String.Empty)
+            {
+                texto1.Text = "Digite seu texto";
+            }
+        }
+    }   
+}
